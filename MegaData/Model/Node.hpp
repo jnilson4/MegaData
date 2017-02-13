1ec2fcaabc09fdaaf423bb95be94792b594893b0
Jake Nilson <j.nilson132@gmail.com>
2017-02-13 12:57:28 -0700

    Completed the Node.hpp file.

--- MegaData/Model/Node.hpp
+++ MegaData/Model/Node.hpp
@@ -24,7 +24,55 @@ public:
     void setNodePointer(Node<Type> pointer);
     Type getNodeData();
     Node<Type> * getNodePointer();
-    
 };
 
-#endif /* Node_h */
+/*
+Implementation Section
+*/
+
+template <class Type>
+Node<Type> :: Node()
+{
+    //Explicitly DO NOT instantiate the nodeData data member
+    this->nodePointer = nullptr;
+}
+
+template <class Type>
+Node<Type> :: Node(Type data)
+{
+    this->nodeData = data;
+    this->nodePointer = nullptr;
+}
+
+template <class Type>
+Node<Type> :: Node(Type data, Node<Type> * pointer)
+{
+    this->nodeData = nodeData;
+    this->nodePointer = nodePointer;
+}
+
+template <class Type>
+void Node<Type> :: setNodeData(Type data)
+{
+    this->nodeData = data;
+}
+
+template <class Type>
+void Node<Type> :: setNodePointer(Node<Type> * nextPointer)
+{
+    this->nodePointer = nextPointer;
+}
+
+
+template <class Type>
+Type node<Type> :: getNodeData()
+{
+    return nodeData;
+}
+
+template <class Type>
+Node<Type> * Node<Type :; getNodePointer()
+{
+    return this->nodePointer;
+}
+#endif /* Node_hpp */
