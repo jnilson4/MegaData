caf05e63ea86712787d7369223c839eedfd97bb9
Jake Nilson <j.nilson132@gmail.com>
2017-02-15 13:02:09 -0700

    Updated the implementation of the array template.

--- MegaData/Model/Array.hpp
+++ MegaData/Model/Array.hpp
@@ -31,5 +31,64 @@ public:
 Implementation Section
 */
 
+template <class Type>
+Array<Type> :: Array()
+{
+    
+}
 
+template <class Type>
+Array<Type> :: Array(int size)
+{
+    assert(size > 0);
+    
+    this->size = size;
+    this->front = new Node<Type>();
+    
+    for(int index = 1; index < size; index++)
+    {
+        Node<Type>() * current = new Node<Type>();
+        current->setNodePointer(front);
+        front = current;
+    }
+}
+
+template <class Type>
+Array<Type> :: setAtIndex(int index, Type data)
+{
+    assert(index >= 0 && index < size);
+    
+    Node<Type>() * current = front;
+    
+    for(int spot = 0; spot < index; spot++)
+    {
+        current = current->getNodePointer();
+    }
+    
+    current->setNodeData(value);
+}
+
+template <class Type>
+Array<Type> :: getFromIndex(int index)
+{
+    assert(index >= 0 && index < size);
+    
+    Type value;
+    
+    Node<Type> * current = front;
+    for(int position = 0; position < index; position++)
+    {
+        current = current->getNodePointer();
+    }
+    
+    value = current->getNodeData();
+    
+    return value;
+}
+
+template <class Type>
+Array<Type> :: getSize()
+{
+    return size;
+}
 #endif /* Array_h */
