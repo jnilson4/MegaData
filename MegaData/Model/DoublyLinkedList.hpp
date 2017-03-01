f875d0debebfbff8e0cea7bf5e727ef7e77e18d3
Jake Nilson <j.nilson132@gmail.com>
2017-03-01 12:51:46 -0700

    Updated the UML and added the DoublyLinkedList

---
+++ MegaData/Model/DoublyLinkedList.hpp
@@ -0,0 +1,20 @@
+//
+//  DoublyLinkedList.hpp
+//  MegaData
+//
+//  Created by Nilson, Jake on 3/1/17.
+//  Copyright © 2017 Nilson. All rights reserved.
+//
+
+#ifndef DoublyLinkedList_h
+#define DoublyLinkedList_h
+
+#include "BiDirectionalNode.hpp"
+
+template <class Type>
+class DoublyLinkedList
+{
+    
+};
+
+#endif /* DoublyLinkedList_h */
