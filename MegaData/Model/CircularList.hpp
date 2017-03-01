9ba168a12820f178fa96c7294d6e5bdd94ed63a3
Jake Nilson <j.nilson132@gmail.com>
2017-03-01 13:51:26 -0700

    Added the CircularList header file and some methods.

---
+++ MegaData/Model/CircularList.hpp
@@ -0,0 +1,40 @@
+//
+//  CircularList.hpp
+//  MegaData
+//
+//  Created by Nilson, Jake on 3/1/17.
+//  Copyright © 2017 Nilson. All rights reserved.
+//
+
+#ifndef CircularList_h
+#define CircularList_h
+
+#include "DoublyLinkedList.hpp"
+
+template <class Type>
+class CircularList : public DoublyLinkedList<Type>
+{
+private:
+    //No Need to redefine the private data members of DoublyLinkedList.
+public:
+    CirularList();
+    ~CirularList();
+    
+    void add(Type data);
+    Type remove(int index);
+    //Other methods as needed :D
+};
+
+template <class Type>
+CircularList<Type> :: CircularList() : DoublyLinkedList()
+{
+    //Deal with circle list stuff only here.
+}
+
+template <class Type>
+void CircularList<Type> :: add(Type data)
+{
+    
+}
+
+#endif /* CircularList_h */
