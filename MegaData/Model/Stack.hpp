a93827ea2259b591993c03f3948ec8e3f167205a
Jake Nilson <j.nilson132@gmail.com>
2017-03-07 13:29:12 -0700

    Added methods to the stack header.

--- MegaData/Model/Stack.hpp
+++ MegaData/Model/Stack.hpp
@@ -9,5 +9,100 @@
 #ifndef Stack_h
 #define Stack_h
 
+#include "DoublyLinkedList.hpp"
+
+template <class Type>
+class Stack : public DoublyLinkedList<Type>
+{
+private:
+public:
+    Stack();
+    ~Stack();
+    void add(Type value);
+    Type remove(int index);
+    Type pop();
+    Type peek();
+    void push(Type data);
+};
+
+template <class Type>
+Stack<Type> :: Stack() : DoublyLinkedList<Type>()
+{
+    
+}
+
+template <class Type>
+Stack<Type> :: ~Stack()
+{
+    BiDirectionalNode<Type> * remove = this->getFront();
+    while(this->getFront() != nullptr)
+    {
+        this->setFront(this->getFront()->getNextPointer());
+        delete remove;
+        remove = this->getFront();
+    }
+}
+
+template <class Type>
+void Stack<Type> :: add(Type valueToAdd)
+{
+    push(valueToAdd);
+}
+
+template <class Type>
+void Stack<Type> :: push(Type addedThing)
+{
+    BiDirectionalNode<Type> * addToStack = new BiDirectionalNode<Type>(addedThing);
+    
+    if(this->getSize() == 0 || this->getFront() == nullptr || this->getEnd() == nullptr)
+    {
+        this->setFront(addToStack);
+    }
+    else
+    {
+        this->getEnd()->setNextPointer(addToStack);
+        addToStack->setPreviousPointer(this->getEnd());
+    }
+    
+    this->setEnd(addToStack);
+    this->setSize(this->getSize() + 1);
+}
+
+template <class Type>
+Type Stack<Type> :: remove(int index)
+{
+    assert(index == this->getSize() -1 && this->getSize() > 0);
+    return pop();
+}
+
+template <class Type>
+Type Stack<Type> :: peek()
+{
+    assert(this->getSize() > 0);
+    return this->getEnd()->setNodeData();
+}
+
+template <class Type>
+Type Stack<Type> :: pop()
+{
+    assert(this->getSize() > 0);
+    Type removed = this->getEnd()->getNodeData();
+    
+    BiDirectionalNode<Type> * update = this->getEnd();
+    update = update->getPreviousPointer();
+    
+    if(update != nullptr)
+    {
+        update->setNextPointer(nullptr);
+    }
+    
+    delete this->getEnd();
+    
+    this->setEnd(update);
+    
+    this->setSize(this->getSize() - 1);
+    
+    return removed;
+}
 
 #endif /* Stack_h */
