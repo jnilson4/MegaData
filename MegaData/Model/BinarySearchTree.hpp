92d4a051f749ebaae4d9200b2703db15274b3972
Jake Nilson <j.nilson132@gmail.com>
2017-04-11 12:59:02 -0600

    Added Tree and BinarySearchTree. Moved the tree file.

---
+++ MegaData/Model/BinarySearchTree.hpp
@@ -0,0 +1,98 @@
+//
+//  BinarySearchTree.hpp
+//  MegaData
+//
+//  Created by Nilson, Jake on 4/11/17.
+//  Copyright © 2017 Nilson. All rights reserved.
+//
+
+#ifndef BinarySearchTree_h
+#define BinarySearchTree_h
+
+#include "Tree.hpp"
+#include "BinarySearchTreeNode.hpp"
+
+template <class Type>
+class BinarySearchTree : public Tree<Type>
+{
+private:
+    BinarySearchTreeNode<Type> * root;
+    
+    int calculateSize(BinarySearchTreeNode<Type> * root);
+    void inOrderTraversal(BinarySearchTreeNode<Type> * inStart);
+    void preOrderTraversal(BinarySearchTreeNode<Type> * preStart);
+    void postOrderTraversal(BinarySearchTreeNode<Type> * postStart);
+    
+public:
+    BinarySearchTree();
+    ~BinarySearchTree();
+    
+    BinarySearchTreeNode<Type> * getRoot();
+    void setRoot(BinarySearchTreeNode<Type> * root);
+    
+    void inOrderTraversal();
+    void preOrderTraversal();
+    void postOrderTraversal();
+};
+
+template <class Type>
+BinarySearchTree<Type> :: BinarySearchTree() : Tree<Type>()
+{
+    this->root = nullptr;
+}
+
+template <class Type>
+BinarySearchTree<Type> :: BinarySearchTree<Type> :: getRoot()
+{
+    return this->root;
+}
+
+template <class Type>
+void BinarySearchTree<Type> :: setRoot(BinarySearchTreeNode<Type> * root)
+{
+    this->root = root;
+}
+
+template <class Type>
+void BinarySearchTree<Type> :: inOrderTraversal()
+{
+    
+}
+
+template <class Type>
+void BinarySeachTree<Type> :: preOrderTraversal()
+{
+    
+}
+
+template <class Type>
+void BinarySearchTree<Type> :: postOrderTraversal()
+{
+    
+}
+
+template <class Type>
+int BinarySearchTree<Type> :: calculateSize(BinarySearchTreeNode<Type> * start)
+{
+    return -99;
+}
+
+template <class Type>
+void BinarySearchTree<Type> :: inOrderTraversal(BinarySearchTreeNode<Type> * inStart)
+{
+    
+}
+
+template <class Type>
+void BinarySearchTree<Type> :: preOrderTraversal(BinarySearchTreeNode<Type> * preStart)
+{
+    
+}
+
+template <class Type>
+void BinarySearchTree<Type> :: postOrderTraversal(BinarySearchTreeNode<Type> * postStart)
+{
+    
+}
+
+#endif /* BinarySearchTree_h */
