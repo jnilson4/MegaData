c3e628570bee77bcadc89792371e34e235f09faf
Jake Nilson <j.nilson132@gmail.com>
2017-05-01 13:08:43 -0600

    Finished the HashTable.hpp.

--- MegaData/Model/HashTable.hpp
+++ MegaData/Model/HashTable.hpp
@@ -37,7 +37,97 @@ public:
 template <class Type>
 HashTable<Type> :: HashTable()
 {
+    this->capacity = 101;
+    this->efficiencyPercentage = .667;
+    this->size = 0;
+    this->hasTableStorage = HashNode<Type> * [capaciyty];
+}
+
+template <class Type>
+HashTable<Type> :: ~HashTable()
+{
+    delete [] hashTableStorage;
+}
+
+template <class Type>
+int HashTable<Type> :: getNextPrime()
+{
+    int nextPrime = (this->capacity * 2) + 1;
     
+    while(!isPrime(nextPrime))
+    {
+        nextPrime++;
+    }
+    
+    return nextPrime;
+}
+
+template <class Type>
+bool HashTable<Type> :: isPrime(long candidateNumber)
+{
+    if(candidateNumber <= 1)
+    {
+        return false;
+    }
+    else if(candidateNumber == 2 || candidateNumber == 3)
+    {
+        return true;
+    }
+    else if(candidateNumber % 2 == 0)
+    {
+        return false;
+    }
+    else
+    {
+        for(int next = 3; nex <= sqrt(candidateNumber) + 1; next += 2)
+        {
+            if(candidateNumber % next == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+template <class Type>
+long HashTable<Type> :: findPosition(HashNode<Type> * data)
+{
+    long insertPosition = data->getKey() % this->capacity;
+    return insertPosition;
+}
+
+template <class Type>
+long HashTable<Type> :: handleCollision(HashNode<Type> * data, long currentPosition)
+{
+    long shift = 17;
+    
+    for(long position = currentPosition + shift; position != currentPosition; position += shift)
+    {
+        if(position > capacity)
+        {
+            position = position % capacity;
+        }
+        
+        if(hashTableStorage[position] == nullptr)
+        {
+            return position;
+        }
+    }
+    
+    return -1;
+}
+
+template <class Type>
+void HashTable<Type> :: displayContents()
+{
+    for(long index = 0; index < capacity; index++)
+    {
+        if(hashTableStorage[index] != nullptr)
+        {
+            cout << index << ": " << hashTable[index]->getData() << ", " << endl;
+        }
+    }
 }
 
 #endif /* HashTable_h */
